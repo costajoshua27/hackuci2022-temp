158746be3061357f47b6ae6cfbda0435b784f4e6
Joshua Costa <costa.joshua27@gmail.com>
2022-02-26 00:48:40 -0800

    Added first start with google api

--- server/app.js
+++ server/app.js
@@ -1,16 +1,24 @@
+const { createServer } = require('http');
 const express = require('express');
 const cors = require('cors');
-const { createServer } = require('http');
-const { Server } = require('socket.io');
 
+const { Server } = require('socket.io');
+const language = require('@google-cloud/language');
+
+// SETUP GOOGLE CLOUD NATURAL LANGUAGE API CLIENT
+const client = new language.LanguageServiceClient();
+
+// SETUP THE EXPRESS APP
 const app = express();
 
 app.use(express.json());
 app.use(express.urlencoded({ extended: true }));
 app.use(cors());
 
+// CREATE THE SERVER
 const server = createServer(app);
 
+// ATTACH THE SOCKET SERVER
 const io = new Server(server, {
   path: '/ws-api',
   cors: {
@@ -26,8 +34,16 @@ app.get('/', (req, res) => {
 
 // DEFINE SOCKET EVENT LISTENERS
 io.on('connect', function(socket) {
-  socket.on('transcription', function(transcription) {
+  socket.on('transcription', async function({ transcription }) {
     console.log(transcription);
+    const [result] = await client.analyzeSentiment({ document: {
+      content: transcription,
+      type: 'PLAIN_TEXT'
+    }});
+    const sentiment = result.documentSentiment;
+    console.log(`Text: ${transcription}`);
+    console.log(`Sentiment score: ${sentiment.score}`);
+    console.log(`Sentiment magnitude: ${sentiment.magnitude}`);
   });
 });
 
