1534274862e6a99d477718b4d33335b0eaad681c
Joshua Costa <costa.joshua27@gmail.com>
2022-02-26 15:01:31 -0800

    Added sessions

--- server/app.js
+++ server/app.js
@@ -4,7 +4,14 @@ const cors = require('cors');
 
 const { Server } = require('socket.io');
 const language = require('@google-cloud/language');
-const { getAudioFileForTranscription } = require('./helpers');
+const { 
+  getAudioFileForTranscription,
+  determineSoundFile,
+  createMasterAudio
+} = require('./helpers');
+
+// SETUP SESSION MAP
+const session = new Map();
 
 // SETUP GOOGLE CLOUD NATURAL LANGUAGE API CLIENT
 const client = new language.LanguageServiceClient();
@@ -29,13 +36,18 @@ const io = new Server(server, {
 });
 
 // DEFINE ROUTES
-app.post('/video/create', (req, res) => {
-  const data = req.data;
+app.post('/video/create', async (req, res) => {
+  const {
+    sessionId
+  } = req.data;
 
-  const images = [];
-  
-  
+  const transcriptData = session.get(sessionId);
 
+  await createMasterAudio(transcriptData, sessionId);
+
+  // CLEANUP
+  session.delete(sessionId);
+  // delete files
 
   return res.status(200).json({
     videoLink: '',
@@ -45,28 +57,22 @@ app.post('/video/create', (req, res) => {
 
 // DEFINE SOCKET EVENT LISTENERS
 io.on('connect', function(socket) {
-  socket.on('transcription', async function({ transcription }) {
-    console.log(transcription);
-    const [resultEntities] = await client.analyzeEntities({  document: {
+  socket.on('transcription', async function({ sessionId, transcription }) {
+    console.log(`Session id: ${sessionId}`);
+    console.log(`Getting information about:\n${transcription}...`);
+
+    const document = {
       content: transcription,
       type: 'PLAIN_TEXT'
-    }});
-    const [resultSentiment] = await client.analyzeSentiment({ document: {
-      content: transcription,
-      type: 'PLAIN_TEXT'
-    }});
-    /*
-    {transcription: string,
-    sound?: string,
-    sentiment: number,
-    entities: Array<entities>}
-    */
-    // Analyze result (what type of sound byte, entities)
+    };
+
+    const [resultEntities] = await client.analyzeEntities({ document });
+    const [resultSentiment] = await client.analyzeSentiment({ document });
+
     const sentiment = resultSentiment.documentSentiment;
     const sentimentScore = sentiment.score;
-    const soundClip = determineSoundClip(sentimentScore);
-    // if we want just the name of the entity
-    const entities = resultEntities.entities.map((currEntity) => {
+
+    const entities = resultEntities.entities.map(currEntity => {
       return {
         name: currEntity.name,
         type: currEntity.type,
@@ -77,35 +83,32 @@ io.on('connect', function(socket) {
     console.log(entities);
     console.log(soundClip);
     console.log(sentimentScore);
-    console.log(transcription);
 
-    socket.emit("transcriptionProcessed", {
-      transcription: transcription,
-      sound: soundClip,
-      sentiment: sentimentScore,
-      entities: entities
+    // INITIALIZE THE SESSION IF IT HASN'T YET
+    if (!session.get(sessionId)) { 
+      session.set(sessionId, []);
+    }
+
+    const currSession = session.get(sessionId);
+    const audioFileName = getAudioFileForTranscription(transcription, sessionId, currSession.length + 1);
+    const soundFileName = determineSoundFile(sentimentScore);
+
+    // CLIENT SHOULD PLAY THE SOUND
+    socket.emit('playSound', {
+      soundFile: soundFileName 
     });
+
+    const transcriptionData = {
+      transcription,
+      audioFile: audioFileName,
+      soundFile: soundFileName
+    };
+
+    currSession.push(transcriptionData);
   });
 });
 
 // HELPER METHODS
-function determineSoundClip(sentimentScore) {
-  if (sentimentScore === 0) {
-    return 'neutral';
-  }
-  else if (sentimentScore >= -0.5 && sentimentScore < 0) {
-    return 'negative';
-  }
-  else if (sentimentScore < -0.5) {
-    return 'strongNegative';
-  }
-  else if (sentimentScore > 0 && sentimentScore <= 0.5) {
-    return 'positive';
-  }
-  else {
-    return 'strongPositive';
-  }
-}
 
 // LISTEN
 server.listen(
--- server/helpers.js
+++ server/helpers.js
@@ -1,23 +1,5 @@
 module.exports = {
-  async getAudioFileForTranscription(transcription) {
-    const textToSpeech = require('@google-cloud/text-to-speech');
-    const client = new textToSpeech.TextToSpeechClient();
-
-    const request = {
-      input: { text: transcription },
-      voice: { languageCode: 'en-US', ssmlGender: 'NEUTRAL' },
-      audioConfig: { audioEncoding: 'MP3' }
-    };
-
-    const [response] = await client.synthesizeSpeech(request);
-
-    const fs = require('fs');
-    const util = require('util');
-
-    const writeFile = util.promisify(fs.writeFile);
-    await writeFile('output.mp3', response.audioContent, 'binary');
-  },
-  determineSoundClip(sentimentScore) {
+  determineSoundFile(sentimentScore) {
     const soundClips = {
       strongNegative: ['GaspV2.mp3'],
       negative: ['GaspV1.mp3', 'SadAww.mp3'],
@@ -52,5 +34,55 @@ module.exports = {
     let randIndex = Math.floor(Math.random()*soundOptions.length);
     console.log(soundOptions[randIndex]);
     return soundOptions[randIndex];
+  },
+  async getAudioFileForTranscription(transcription, sessionId, n) {
+    const textToSpeech = require('@google-cloud/text-to-speech');
+    const client = new textToSpeech.TextToSpeechClient();
+
+    const request = {
+      input: { text: transcription },
+      voice: { languageCode: 'en-US', ssmlGender: 'NEUTRAL' },
+      audioConfig: { audioEncoding: 'MP3' }
+    };
+
+    const [response] = await client.synthesizeSpeech(request);
+
+    const fs = require('fs');
+    const util = require('util');
+    const filename = require(`./tmp/${sessionId}-${n}.mp3`);
+
+    const writeFile = util.promisify(fs.writeFile);
+    await writeFile(filename, response.audioContent, 'binary');
+
+    return filename;
+  },
+  async getStockImageForTranscription(transcription) {
+
+  },
+  createMasterAudio(transcriptData, sessionId) {
+    const audioconcat = require('audioconcat');
+
+    const audioFiles = [];
+    for (const data of transcriptData) {
+      audioFiles.push(data.audioFile);
+      if (data.soundFile) {
+        audioFiles.push(data.soundFile);
+      }
+    }
+
+    return new Promise((resolve, reject) => {
+      audioconcat(audioFiles)
+        .concat(`./tmp/${sessionId}.mp3`)
+        .on('start', function (command) {
+          console.log('ffmpeg process started:', command);
+        })
+        .on('error', function (err) {
+          reject(new Error(err));
+        })
+        .on('end', function (output) {
+          console.log('Audio created in:', output);
+          resolve();
+        });
+    });
   }
 };
