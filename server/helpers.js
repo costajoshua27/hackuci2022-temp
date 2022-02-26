6daf1c1904180a3f3108cdc32160411d84bd270d
Jhuangia <jialh1@uci.edu>
2022-02-26 14:49:22 -0800

    Update determineSoundClip, returns name of soundClip

--- server/helpers.js
+++ server/helpers.js
@@ -16,5 +16,41 @@ module.exports = {
 
     const writeFile = util.promisify(fs.writeFile);
     await writeFile('output.mp3', response.audioContent, 'binary');
+  },
+  determineSoundClip(sentimentScore) {
+    const soundClips = {
+      strongNegative: ['GaspV2.mp3'],
+      negative: ['GaspV1.mp3', 'SadAww.mp3'],
+      positive:['Impressed_Ooh.mp3', 'LaughV2.mp3'],
+      strongPositive: ['Celebrity_Intro.mp3', 'Intrigued_Ooh.mp3']
+    };
+
+    let sentiment;
+    if (sentimentScore > -0.2 && sentimentScore < 0.2) {
+      sentiment = 'neutral';
+    }
+    else if (sentimentScore >= -0.6 && sentimentScore < -0.2) {
+      sentiment = 'negative';
+    }
+    else if (sentimentScore < -0.6) {
+      sentiment = 'strongNegative';
+    }
+    else if (sentimentScore > 0.2 && sentimentScore <= 0.6) {
+      sentiment = 'positive';
+    }
+    else {
+      sentiment = 'strongPositive';
+    }
+
+    if (sentiment === 'neutral') {
+      return null;
+    }
+    
+    let soundOptions = soundClips[sentiment];
+    console.log(sentimentScore);
+    console.log(soundOptions);
+    let randIndex = Math.floor(Math.random()*soundOptions.length);
+    console.log(soundOptions[randIndex]);
+    return soundOptions[randIndex];
   }
 };
